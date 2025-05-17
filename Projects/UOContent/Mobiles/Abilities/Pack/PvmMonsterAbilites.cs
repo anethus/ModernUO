bbe16d6bc6261df8c557d3838e556896b5c344e2
anethus <dyru@poczta.fm>
2025-05-17 23:36:21 +0200

    FewNewSkills

--- Projects/UOContent/Mobiles/Abilities/Pack/PvmMonsterAbilites.cs
+++ Projects/UOContent/Mobiles/Abilities/Pack/PvmMonsterAbilites.cs
@@ -11,6 +11,9 @@ using Server.Engines.BuffIcons;
 using System.Linq;
 using Server.Spells.Spellweaving;
 using Server.Spells.Fourth;
+using System.Drawing;
+using Server.Targeting;
+using System.Numerics;
 
 namespace Server.Custom.PvM
 {
@@ -23,30 +26,239 @@ namespace Server.Custom.PvM
 
         public static void Configure()
         {
-            CommandSystem.Register("AbilityBlizzard", AccessLevel.Administrator, e => Blizzard(e.Mobile, e.Mobile.Location, 8, 1000, 10));
-            CommandSystem.Register("AbilityFirebomb", AccessLevel.Administrator, e => ThrowFirebomb(e.Mobile, new Point3D(e.Mobile.X, e.Mobile.Y + 6, e.Mobile.Z), true, TimeSpan.FromSeconds(10), null));
-            //CommandSystem.Register("AbilityBarrel", AccessLevel.Administrator, e => ThrowBarrel(e.Mobile, new Point3D(e.Mobile.X, e.Mobile.Y + 6, e.Mobile.Z), true, TimeSpan.FromSeconds(10), null));
-            //CommandSystem.Register("AbyssalRift", AccessLevel.Administrator, e => AbyssalRift(e.Mobile));
-            CommandSystem.Register("AbyssalNuke", AccessLevel.Administrator, e => AbyssalNuke(e.Mobile));
-            CommandSystem.Register("ChiefBelch", AccessLevel.Administrator, e => ChiefBelch(e.Mobile));
-            CommandSystem.Register("ChiefCough", AccessLevel.Administrator, e => ChiefCough(e.Mobile));
-            CommandSystem.Register("CrystalShards", AccessLevel.Administrator, e => CrystalShards(e.Mobile));
-            CommandSystem.Register("CrystalBleed", AccessLevel.Administrator, e => CrystalBleed(e.Mobile));
-            CommandSystem.Register("HoofStomp", AccessLevel.Administrator, e => HoofStomp(e.Mobile));
-            CommandSystem.Register("TeleportAttack", AccessLevel.Administrator, e => TeleportAttack(e.Mobile));
-            //CommandSystem.Register("CorruptVirtue", AccessLevel.Administrator, e => CorruptVirtue(e.Mobile));
-            //CommandSystem.Register("TakeLife", AccessLevel.Administrator, e => TakeLife(e.Mobile));
-            //CommandSystem.Register("AreaPainspike", AccessLevel.Administrator, e => AreaPainspike(e.Mobile));
-            //CommandSystem.Register("SilenceHowl", AccessLevel.Administrator, e => SilenceHowl(e.Mobile));
-            //CommandSystem.Register("SpillAcid", AccessLevel.Administrator, e => SpillAcid(e.Mobile, e.Mobile));
-            CommandSystem.Register("WaterWaves", AccessLevel.Administrator, e => WaterWaves(e.Mobile));
-            CommandSystem.Register("WaterClouds", AccessLevel.Administrator, e => WaterClouds(e.Mobile));
-            //CommandSystem.Register("SummonHelpers", AccessLevel.Administrator, e => SummonHelpers(e.Mobile));
-            CommandSystem.Register("PhantasmicMockery", AccessLevel.Administrator, e => PhantasmicMockery(e.Mobile));
-            CommandSystem.Register("ChillWinds", AccessLevel.Administrator, e => ChillWinds(e.Mobile));
-            CommandSystem.Register("TrapScattering", AccessLevel.Administrator, e => TrapScattering(e.Mobile));
+            // CommandSystem.Register("AbilityBlizzard", AccessLevel.Administrator, e => Blizzard(e.Mobile, e.Mobile.Location, 8, 1000, 10));
+            // CommandSystem.Register("AbilityFirebomb", AccessLevel.Administrator, e => ThrowFirebomb(e.Mobile, new Point3D(e.Mobile.X, e.Mobile.Y + 6, e.Mobile.Z), true, TimeSpan.FromSeconds(10), null));
+            // //CommandSystem.Register("AbilityBarrel", AccessLevel.Administrator, e => ThrowBarrel(e.Mobile, new Point3D(e.Mobile.X, e.Mobile.Y + 6, e.Mobile.Z), true, TimeSpan.FromSeconds(10), null));
+            // //CommandSystem.Register("AbyssalRift", AccessLevel.Administrator, e => AbyssalRift(e.Mobile));
+            // CommandSystem.Register("AbyssalNuke", AccessLevel.Administrator, e => AbyssalNuke(e.Mobile));
+            // CommandSystem.Register("ChiefBelch", AccessLevel.Administrator, e => ChiefBelch(e.Mobile));
+            // CommandSystem.Register("ChiefCough", AccessLevel.Administrator, e => ChiefCough(e.Mobile));
+            // CommandSystem.Register("CrystalShards", AccessLevel.Administrator, e => CrystalShards(e.Mobile));
+            // CommandSystem.Register("CrystalBleed", AccessLevel.Administrator, e => CrystalBleed(e.Mobile));
+            // CommandSystem.Register("HoofStomp", AccessLevel.Administrator, e => HoofStomp(e.Mobile));
+            // CommandSystem.Register("TeleportAttack", AccessLevel.Administrator, e => TeleportAttack(e.Mobile));
+            // //CommandSystem.Register("CorruptVirtue", AccessLevel.Administrator, e => CorruptVirtue(e.Mobile));
+            // //CommandSystem.Register("TakeLife", AccessLevel.Administrator, e => TakeLife(e.Mobile));
+            // //CommandSystem.Register("AreaPainspike", AccessLevel.Administrator, e => AreaPainspike(e.Mobile));
+            // //CommandSystem.Register("SilenceHowl", AccessLevel.Administrator, e => SilenceHowl(e.Mobile));
+            // //CommandSystem.Register("SpillAcid", AccessLevel.Administrator, e => SpillAcid(e.Mobile, e.Mobile));
+            // CommandSystem.Register("WaterWaves", AccessLevel.Administrator, e => WaterWaves(e.Mobile));
+            // CommandSystem.Register("WaterClouds", AccessLevel.Administrator, e => WaterClouds(e.Mobile));
+            // //CommandSystem.Register("SummonHelpers", AccessLevel.Administrator, e => SummonHelpers(e.Mobile));
+            // CommandSystem.Register("PhantasmicMockery", AccessLevel.Administrator, e => PhantasmicMockery(e.Mobile));
+            // CommandSystem.Register("ChillWinds", AccessLevel.Administrator, e => ChillWinds(e.Mobile));
+            // CommandSystem.Register("TrapScattering", AccessLevel.Administrator, e => TrapScattering(e.Mobile));
             CommandSystem.Register("TrapScattering", AccessLevel.Administrator, e => TrapScattering(e.Mobile));
             CommandSystem.Register("HeartwarmingSong", AccessLevel.Administrator, e => HeartwarmingSong(e.Mobile));
+            CommandSystem.Register("OrcCleave", AccessLevel.Administrator, e => OrcCleave(e.Mobile));
+            CommandSystem.Register("CageTrapp", AccessLevel.Administrator, e => CageTrap(e.Mobile));
+
+        }
+
+        public static void CageTrap(Mobile from)
+        {
+            if (!from.Alive || from.Map == null || from.Deleted || from.Map == Map.Internal)
+                return;
+
+            from.PublicOverheadMessage(MessageType.Emote, 1154, true, $"*looking for a person to trap*");
+
+            Timer.DelayCall(TimeSpan.FromSeconds(1.5), () =>
+            {
+                using var pool = PooledRefQueue<Mobile>.Create();
+                var eable = from.GetMobilesInRange(10);
+
+                foreach (var target in eable)
+                {
+                    if (target.Serial == from.Serial || !target.Alive || target.Map == null || target.Deleted)
+                    {
+                        continue;
+                    }
+
+                    pool.Enqueue(target);
+                }
+
+                if (pool.Count <= 0)
+                {
+                    return;
+                }
+
+                var toFreeze = pool.PeekRandom();
+                var mTrap = new PrisoneTrap(toFreeze);
+                mTrap.MoveToWorld(toFreeze.Location, from.Map);
+            });
+        }
+
+        [SerializationGenerator(0, false)]
+        public partial class PrisoneTrap : BaseTrap
+        {
+            private int hp = 50;
+            private int _freezeTimer = 0;
+            private Mobile _freezePersone;
+
+            public override int PassiveTriggerRange => 1;
+            public override bool CanTarget => base.CanTarget;
+
+            [Constructible]
+            public PrisoneTrap(Mobile toFreeze) : base(0x21FC)
+            {
+                _freezeTimer = Utility.RandomMinMax(40, 140);
+                
+                Timer.DelayCall(TimeSpan.FromSeconds(_freezeTimer), Explode);
+
+                _freezePersone = toFreeze;
+                _freezePersone.Freeze(TimeSpan.FromSeconds(_freezeTimer));
+            }
+
+            public override void OnDelete()
+            {
+                base.OnAfterDelete();
+                if (_freezePersone != null)
+                {
+                    _freezePersone.Frozen = false;
+                }
+            }
+
+            private void Explode()
+            {
+                Effects.PlaySound(Location, Map, 0x306);
+
+                Delete();
+            }
+
+            public void DoDamage()
+            {
+                hp -= 10;
+
+                PublicOverheadMessage(MessageType.Emote, 0x33, true, "*start breaking a trap*");
+                if (hp <= 0)
+                {
+                    Delete();
+                }
+            }
+        }
+
+        [SerializationGenerator(0, false)]
+        public partial class SpearTrapBreaker : Item
+        {
+            public override bool CanEquip(Mobile m)
+            {
+                return false;
+            }
+
+            [Constructible]
+            public SpearTrapBreaker() : base(0x2555)
+            {
+
+            }
+
+            public override void OnDoubleClick(Mobile from)
+            {
+                base.OnDoubleClick(from);
+
+                from.SendMessage("Target Prison");
+                from.Target = new SpearItemTarget();
+            }
+        }
+
+
+        public class SpearItemTarget : Target
+        {
+            public SpearItemTarget() : base(5, false, TargetFlags.None)
+            {
+            }
+
+            protected override void OnTarget(Mobile from, object targeted)
+            {
+                if (targeted is PrisoneTrap pt)
+                {
+                    pt.DoDamage();
+                }
+            }
+        }
+
+        public static void OrcCleave(Mobile from)
+        {
+            if (!from.Alive || from.Map == null || from.Deleted || from.Map == Map.Internal)
+                return;
+
+            from.PublicOverheadMessage(MessageType.Emote, 1154, true, $"*start swinging the sword*");
+            from.Animate(9, 5, 3, true, false, 0);
+            from.PlaySound(0x2A);
+
+            Timer.DelayCall(TimeSpan.FromSeconds(3.5), () =>
+            {
+                new OrcCleaveTimer(from).Start();
+            });
+        }
+
+        private class OrcCleaveTimer : Timer
+        {
+            private Mobile _from;
+            public OrcCleaveTimer(Mobile from) : base(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(2), 30)
+            {
+                _from = from;
+            }
+
+            protected override void OnTick()
+            {
+                if (!_from.Alive || _from.Deleted)
+                {
+                    Stop();
+                    return;
+                }
+                _from.Animate(9, 5, 3, true, false, 0);
+                _from.PlaySound(0x2A);
+
+                var position = new Vector2(_from.X, _from.Y);
+                var dirVector = GetNormalizedLookDirection(_from.Direction);
+
+                var enemies = _from.Map.GetMobilesInRange(_from.Location, 10);
+
+                foreach (var enemy in enemies)
+                {
+                    if (enemy.Serial == _from.Serial)
+                    {
+                        continue;
+                    }
+
+                    var canSee = CheckObjectInFov(new Vector2(enemy.X, enemy.Y), position, dirVector, 45f);
+
+                    if (canSee)
+                    {
+                        SpellHelper.Damage(TimeSpan.FromSeconds(0.5), _from, _from, Utility.Dice(2, 4, 0));
+                    }
+                }
+            }
+        }
+
+        public static bool CheckObjectInFov(Vector2 targetPosition, Vector2 fromPosition, Vector2 vectorLookDirection, float viewRadius)
+        {
+            Vector2 vectorToObject = targetPosition - fromPosition;
+
+            float angleToObjectRad = MathF.Atan2(vectorToObject.Y, vectorToObject.X) - MathF.Atan2(vectorLookDirection.Y, vectorLookDirection.X);
+
+            while (angleToObjectRad > MathF.PI) angleToObjectRad -= 2 * MathF.PI;
+            while (angleToObjectRad < -MathF.PI) angleToObjectRad += 2 * MathF.PI;
+
+            float fieldOfViewRad = viewRadius * 0.5f * MathF.PI / 180f;
+
+            return MathF.Abs(angleToObjectRad) <= fieldOfViewRad;
+        }
+
+        public static Vector2 GetNormalizedLookDirection(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.North => new Vector2(0, -1),
+                Direction.South => new Vector2(0, 1),
+                Direction.West => new Vector2(-1, 0),
+                Direction.East => new Vector2(1, 0),
+                Direction.Down => new Vector2(1, 1),
+                Direction.Up => new Vector2(-1, -1),
+                Direction.Right => new Vector2(1, -1),
+                Direction.Left => new Vector2(-1, 1),
+                _ => Vector2.Zero,
+            };
         }
 
         public static void HeartwarmingSong(Mobile from)
@@ -55,10 +267,10 @@ namespace Server.Custom.PvM
                 return;
 
             from.PublicOverheadMessage(MessageType.Emote, 1154, true, $"*start screaming*");
-            
+
             from.Animate(9, 5, 3, true, false, 0);
             from.PlaySound(0x2A);
-            
+
             Timer.DelayCall(TimeSpan.FromSeconds(3.5), () =>
             {
                 var summons = from.Map.GetMobilesInRange<BaseCreature>(from.Location, 15);
@@ -1820,6 +2032,27 @@ namespace Server.Custom.PvM
             }
 
             return true;
+        }
+
+        public static bool IsPointInTrapezoid(Point2D p, Point2D a, Point2D b, Point2D c, Point2D d)
+        {
+            static double CrossProduct(Point2D v1, Point2D v2)
+            {
+                return v1.X * v2.Y - v1.Y * v2.X;
+            }
+
+            static Point2D Subtract(Point2D p1, Point2D p2)
+            {
+                return new Point2D { X = p1.X - p2.X, Y = p1.Y - p2.Y };
+            }
+
+            double cp1 = CrossProduct(Subtract(b, a), Subtract(p, a));
+            double cp2 = CrossProduct(Subtract(c, b), Subtract(p, b));
+            double cp3 = CrossProduct(Subtract(d, c), Subtract(p, c));
+            double cp4 = CrossProduct(Subtract(a, d), Subtract(p, d));
+
+            return (cp1 >= 0 && cp2 >= 0 && cp3 >= 0 && cp4 >= 0) ||
+                (cp1 <= 0 && cp2 <= 0 && cp3 <= 0 && cp4 <= 0);
         }
     }
 }
